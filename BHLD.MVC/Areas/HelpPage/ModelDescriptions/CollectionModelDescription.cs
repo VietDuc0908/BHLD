7e0fa3c6c2a736d5d10f1bf3a488f0b2a39e1636
VietDuc0908 <86755488+VietDuc0908@users.noreply.github.com>
2021-08-10 16:30:40 +0700

    duc add kendo

---
+++ BHLD.MVC/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BHLD.MVC.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BHLD.MVC/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BHLD.MVC.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
