744dbf9d0e9ef550d86bb1640f11ab1c72075f1d
VietDuc0908 <86755488+VietDuc0908@users.noreply.github.com>
2021-08-10 15:48:03 +0700

    3rd commit

---
+++ BHLD.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHLD.Data.Infrastructure
+{
+    public class DbFactory : IDbFactory
+    {
+        private bhldDBContext dbContext;
+    }
+}
---
+++ BHLD.Data/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHLD.Data.Infrastructure
+{
+    public abstract class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        private IDbFactory dbFactory;
+
+        protected RepositoryBase(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+    }
+}
