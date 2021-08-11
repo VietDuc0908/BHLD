bbc38eee2d990c0f31cf3d2119a2b750064dc64f
NguyennYen <yenle210399@gmail.com>
2021-08-12 00:31:28 +0700

    Thin test api hu-title

--- BHLD.MVC/Controllers/HomeController.cs
+++ BHLD.MVC/Controllers/HomeController.cs
@@ -1,17 +1,43 @@
-﻿using System;
+﻿using BHLD.Model.Models;
+using BHLD.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace BHLD.MVC.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+        private hu_titleServices hu_title_service = new hu_titleServices();
+        // GET: Hu_title
+        [System.Web.Http.HttpGet]
+        public IEnumerable<hu_title> GetAllList()
+        {
+            return hu_title_service.GetAll();
+        }
+        [System.Web.Http.HttpPost]
+        public bool Post(hu_title title)
+        {
+            if (title != null)
+            {
+                hu_title_service.Add(title);
+                return true;
+            }
+            return false;
+        }
+        [System.Web.Http.HttpDelete]
+        public bool Delete(int id)
+        {
+            return false;
+
         }
     }
 }
---
+++ BHLD.MVC/Static/js/hu_title.js
@@ -0,0 +1,35 @@
+﻿const apiUrl = "/api/";
+
+async function CallApi(endpoint, method = "GET", body = null) {
+    var url = apiUrl + endpoint;
+    const resp = await fetch(url, {
+        method,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body
+    });
+    return resp.json();
+}
+
+
+CallApi("hu_tilte").then(res => {
+    if (res) {
+        const SELECT_HU_TITLE = document.getElementById("hu_title");
+        res.forEach(tittle => {
+            const OPTIONELM = `
+<td>${tittle.code }</td>
+<td>${tittle.title_name}</td>
+<td>${tittle.remark }</td>
+<td>${tittle.actflg }</td>
+<td>${tittle.created_by }</td>
+<td>${tittle.created_date }</td>
+<td>${tittle.created_log}</td>
+<td>${tittle.modified_date }</td>
+<td>${tittle.status   }</td>`;
+
+
+            SELECT_HU_TITLE.insertAdjacentHTML("beforeend", OPTIONELM);
+        })
+    }
+})
